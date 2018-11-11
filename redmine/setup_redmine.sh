0446429bbaf1530d9ee7d100a150246caf477bc6
Satoru MIYAZAKI <s.miyaza@gmail.com>
2018-11-11 21:47:47 +0900

    modify spell miss

--- redmine/setup_redmine.sh
+++ redmine/setup_redmine.sh
@@ -10,7 +10,7 @@ export DBPASS=`mkpasswd -s 0 -l 20`
 export EMAILOPTION=--no-email
 
 yum install -y ImageMagick-devel
-kusanagi init --tz tokyo --lang en --keyboard en --passwd $KUSANAGI_PASSWORD --no-phrase --dbrootpass "$DBROOTPASS" --php7 --nginx --ruby24 --dbsystem mariadb
+test -f /etc/kusanagi.d/profile.conf || (echo n | kusanagi init --tz tokyo --lang en --keyboard en --passwd $KUSANAGI_PASSWORD --no-phrase --dbrootpass "$DBROOTPASS" --php7 --nginx --ruby24 --dbsystem mariadb)
 kusanagi provision --rails --fqdn $SITE_DOMAIN --noemail --dbname $DBNAME --dbuser $DBUSER --dbpass "$DBPASS" $KUSANAGI_PROFILE
 rm -rf /home/kusanagi/$KUSANAGI_PROFILE
 systemctl stop nginx
@@ -27,8 +27,8 @@ chown -R kusanagi:www log
 chmod 775 log
 echo set rails configure
 cp /tmp/ar_innodb_row_format.rb config/initializers/ar_innodb_row_format.rb
-echo 'config.logger = Logger.new(Rails.root.join("log",Rails.env + "-ERROR.log"), 3, 10 * 1024 * 1024)' | tee config/additional_environment.rb > /dev/null
-echo 'config.logger.level = Logger::ERROR' | tee -a config/additional_environment.rb > /dev/null
+echo 'config.logger = Logger.new(Rails.root.join("log",Rails.env + "-ERROR.log"), 3, 10 * 1024 * 1024)' >> config/additional_environment.rb
+echo 'config.logger.level = Logger::ERROR' >> config/additional_environment.rb
 cp config/configuration.yml.example config/configuration.yml
 sed -e "s/database: .*$/database: $DBNAME/" -e "s/username: .*$/username: $DBUSER/" -e "s/password: .*$/password: \"$DBPASS\"/"  -e "s/encoding: .*$/encoding: utf8mb4/" config/database.yml.example config/database.yml.example | tee config/database.yml > /dev/null
 
@@ -41,7 +41,7 @@ systemctl start mysql
 echo set nginx settings
 awk '/passenger_min_instances/ {print "\t\tpassenger_user httpd;\n\t\tpassenger_group www;"} /rails_env development;/ { printf "#"} /rails_env production;/ {print "\t\trails_env production;"} {print}' /etc/nginx/conf.d/${KUSANAGI_PROFILE}_http.conf > /tmp/${KUSANAGI_PROFILE}_http.conf && cat /tmp/${KUSANGI_PROFILE}_http.conf > /etc/nginx/conf.d/${KUSANAGI_PROFILE}_http.conf && rm /tmp/${KUSANAGI_PROFILE}_http.conf
 awk '/passenger_min_instances/ {print "\t\tpassenger_user httpd;\n\t\tpassenger_group www;"} /rails_env development;/ { printf "#"} /rails_env production;/ {print "\t\trails_env production;"} {print}' /etc/nginx/conf.d/${KUSANAGI_PROFILE}_ssl.conf > /tmp/${KUSANAGI_PROFILE}_ssl.conf && cat /tmp/${KUSANGI_PROFILE}_ssl.conf > /etc/nginx/conf.d/${KUSANAGI_PROFILE}_ssl.conf && rm /tmp/${KUSANAGI_PROFILE}_ssl.conf
-grep -v 'passenter_instance_registry_dir' && echo 'passenger_instance_registry_dir /var/run/passenger-instreg;' >> /etc/nginx/conf.d/kusanagi_rails.conf
+grep -v 'passenter_instance_registry_dir' /etc/nginx/conf.d/kusanagi_rails.conf || echo 'passenger_instance_registry_dir /var/run/passenger-instreg;' >> /etc/nginx/conf.d/kusanagi_rails.conf
 
 systemctl start nginx
 
